7679133980c8e153ad0246545c3ddac74b544f1b
junkRatte <59822502+junkRatte@users.noreply.github.com>
2021-11-22 10:21:18 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter basename="/">
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <App />
     </HashRouter>
   </React.StrictMode>,
